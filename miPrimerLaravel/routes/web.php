1ddeef08609847e58717ec9dea79bf3c25cd8fad
marcos <marcoscav@gmail.com>
2018-11-16 09:01:41 -0300

    iniciando mi Primer Laravel

---
+++ miPrimerLaravel/routes/web.php
@@ -0,0 +1,84 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+function sarasa($chau) {
+
+  $datosPeli = [
+   ['title' => 'Avatar', 'poster' => 'images/avatar.jpg' , 'genre' => 'Ciencia Ficción'],
+   ['title' => 'Infinity War', 'poster' => 'images/avengers.jpg' , 'genre' => 'Acción'],
+   ['title' => 'DeadPool', 'poster' => 'images/deadpool1.jpg' , 'genre' => 'Acción'],
+   ['title' => 'Dragon Ball', 'poster' => 'images/dragonball.jpg' , 'genre' => 'Animé'],
+   ['title' => 'Dunkerque', 'poster' => 'images/dunkirk.jpg' , 'genre' => 'Belico'],
+   ['title' => 'Emoji', 'poster' => 'images/emoji.jpg' , 'genre' => 'Animada'],
+   ['title' => 'Inception', 'poster' => 'images/inception1.jpg' , 'genre' => 'Drama'],
+   ['title' => 'Moana', 'poster' => 'images/moana.jpg' , 'genre' => 'Animada'],
+   ['title' => 'Rogue One', 'poster' => 'images/rogueone.jpg' , 'genre' => 'Acción'],
+   ['title' => 'Titanic','poster' => 'images/titanic.jpg' , 'genre' => 'Drama'],
+   ];
+
+  $verificar=false;
+
+  foreach ($datosPeli as $datos) {
+    if($datos['title'] == $chau) {
+      echo $datos['title'];
+      echo $datos['genre'];
+      $verificar=true;
+    }
+  }
+
+  if($verificar==false) {
+    echo "no se encontró la película";
+  }
+
+}
+
+Route::get('/pelicula/{chau}', function($chau) {
+    sarasa($chau);
+});
+
+
+function hola() {
+  $peliculas = [
+   ['title' => 'Avatar', 'poster' => 'images/avatar.jpg' , 'genre' => 'Ciencia Ficción'],
+   ['title' => 'Infinity War', 'poster' => 'images/avengers.jpg' , 'genre' => 'Acción'],
+   ['title' => 'DeadPool', 'poster' => 'images/deadpool1.jpg' , 'genre' => 'Acción'],
+   ['title' => 'Dragon Ball', 'poster' => 'images/dragonball.jpg' , 'genre' => 'Animé'],
+   ['title' => 'Dunkerque', 'poster' => 'images/dunkirk.jpg' , 'genre' => 'Belico'],
+   ['title' => 'Emoji', 'poster' => 'images/emoji.jpg' , 'genre' => 'Animada'],
+   ['title' => 'Inception', 'poster' => 'images/inception1.jpg' , 'genre' => 'Drama'],
+   ['title' => 'Moana', 'poster' => 'images/moana.jpg' , 'genre' => 'Animada'],
+   ['title' => 'Rogue One', 'poster' => 'images/rogueone.jpg' , 'genre' => 'Acción'],
+   ['title' => 'Titanic','poster' => 'images/titanic.jpg' , 'genre' => 'Drama'],
+   ];
+
+   foreach ($peliculas as $peli) {
+   echo '<li>'.$peli['title'].' - '.$peli['genre'];
+   echo '<br> <img src='.$peli['poster'].'> </li>';
+   };
+}
+
+Route::get('/peliculas', function () {
+    echo '<h1>Listado de Pelis</h1>';
+    echo'<ul>';
+    hola();
+    echo '</ul>';
+
+});
+
+
+Route::get('/miRuta', function () {
+    return '<h1>"Mi primer ruta en Laravel"</h1>';
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
